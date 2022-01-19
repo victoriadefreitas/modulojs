437249e17eef8594a1b63695db76fd25a2846154
Victoria de Freitas <vickgfreitas@gmail.com>
2022-01-19 15:27:31 -0300

    Modifiquei a pasta 4

---
+++ 4-typesConversion.js
@@ -0,0 +1,13 @@
+console.log ("Types Conversion");
+
+console.log ("ano" + 2020);
+console.log ("2" + "2");
+console.log (parseInt("2") + parseInt("2"));
+console.log ("10" / "2")
+console.log ("Victória" + "3") //not a nunmber
+console.log (6.5)
+
+
+ 
+ 
+ 
