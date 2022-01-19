1afc60b8a6537929b1e8eb24675276596b4da6d4
Victoria de Freitas <vickgfreitas@gmail.com>
2022-01-19 14:03:41 -0300

    Create archive 'myfirstprogram'

---
+++ 1-myfirstprogram.js
@@ -0,0 +1,11 @@
+console.log ("My first program! Working with variables ");
+
+const age = 26;
+ console.log(age);
+ console.log(age+2);
+ console.log(age-2);
+ console.log(age/2);
+
+ const summedage = age+4;
+ console.log (summedage);
+ 
