73cb642879122265182291dea060580c777f97a1
Victoria de Freitas <vickgfreitas@gmail.com>
2022-01-22 11:29:41 -0300

    Alteração do arquivo 5

---
+++ 5-assigmentsVariable.js
@@ -0,0 +1,15 @@
+console.log ("Working with assigment variabel");
+
+const age = 29;
+let noun = "Vica";
+const surname = "Freitas";
+
+console.log (noun, surname);
+console.log (`Meu nome é ${noun}`);
+
+noun = noun + surname;
+
+let idade;
+idade = 26;
+idade = idade + 1;
+console.log (idade);
