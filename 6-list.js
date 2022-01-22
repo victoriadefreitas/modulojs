9f8c214e6d60bedd95c60d1820f3bff699323feb
Victoria de Freitas <vickgfreitas@gmail.com>
2022-01-22 11:54:53 -0300

    Incluindo a pasta 6 e array

---
+++ 6-list.js
@@ -0,0 +1,17 @@
+const { Console } = require("console");
+
+console.log (`working with lists`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const destinationsList = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+destinationsList.push (`Curitiba`) //adicionando a uma lista
+console.log (`Possible destinations`);
+//console.log (salvador, saoPaulo, rioDeJaneiro);
+console.log (destinationsList);
